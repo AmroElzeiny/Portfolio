3996410e89cd12dec7f34f86118011dda0c39944
AmroElzeiny <79336744+AmroElzeiny@users.noreply.github.com>
2023-01-26 17:22:35 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,23 @@
 export const projects = [
   {
-    title: 'Segafredo',
-    description: "Using HTML, CSS, and JS - from start to finish. The website is called Segafredo and it is a international coffee company.",
-      image: 'images/segafredo.png',
-      tags: ['HTML', 'CSS', 'JS'],
-    source: 'https://www.figma.com/file/1XWyZkYCTm34ukEGk0OiUQ/Segafredo---home-page-design?node-id=0%3A1&t=UWkaXCBHXDBSgkk8-0',
-    visit: 'https://github.com/AmroElzeiny/segafredo/blob/main/Untitled-1.html',
-    id: 0,
-  },
-  {
     title: 'Portfolio',
     description:"Using HTML, CSS, JS, and React - from start to finish. This is my Portfolio and it is a fully responsive design built professionally.",
     image: 'images/portfolio.png',
     tags: ['HTML', 'CSS', 'JS', 'React'],
     source: 'https://github.com/AmroElzeiny/Portfolio',
     visit: 'https://amroelzeiny.github.io/Portfolio/',
+    id: 0,
+  },
+  {
+    title: 'Segafredo',
+    description: "Using HTML, CSS, and JS - from start to finish. The website is called Segafredo and it is a international coffee company.",
+      image: 'images/segafredo.png',
+      tags: ['HTML', 'CSS', 'JS'],
+    source: 'https://www.figma.com/file/1XWyZkYCTm34ukEGk0OiUQ/Segafredo---home-page-design?node-id=0%3A1&t=UWkaXCBHXDBSgkk8-0',
+    visit: 'https://github.com/AmroElzeiny/segafredo/blob/main/Untitled-1.html',
     id: 1,
   }
+  
 ];
 
 export const TimeLineData = [
