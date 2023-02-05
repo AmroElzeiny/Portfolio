19372d237e0a39d70f13430c85cb561fbf108ad2
AmroElzeiny <79336744+AmroElzeiny@users.noreply.github.com>
2023-02-06 00:26:27 +0200

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -13,8 +13,8 @@ export const projects = [
     description: "Using HTML, CSS, and JS - from start to finish. The website is called Segafredo and it is a international coffee company.",
       image: 'images/segafredo.png',
       tags: ['HTML', 'CSS', 'JS'],
-    source: 'https://www.figma.com/file/1XWyZkYCTm34ukEGk0OiUQ/Segafredo---home-page-design?node-id=0%3A1&t=UWkaXCBHXDBSgkk8-0',
-    visit: 'https://github.com/AmroElzeiny/segafredo/blob/main/Untitled-1.html',
+    source: 'https://github.com/AmroElzeiny/segafredo/blob/main/Untitled-1.html',
+    visit: 'https://amroelzeiny.github.io/Segafredo-test/',
     id: 1,
   }
   
